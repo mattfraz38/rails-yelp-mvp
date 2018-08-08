36d1de93668978591e0aa4c3c5ee03e4dac24775
Matthew Frazier <frazier3388@yahoo.com>
2018-08-08 15:42:17 +0900

    Completed index show new form NO create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Yoshinoya',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '215-651-6524',
+    category:     'japanese'
+  },
+  {
+    name:         'Pizza Hut',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '215-651-6524',
+    category:     'italian'
+
+  },
+  {
+    name:         'Golden Corral',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '215-651-6524',
+    category:     'chinese'
+  },
+  {
+    name:         'Texas Roadhouse',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '215-651-6524',
+    category:     'belgian'
+  },
+  {
+    name:         'Subway',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '215-651-6524',
+    category:     'french'
+  },
+  {
+    name:         'Wendy\'s',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '215-651-6524',
+    category:     'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
